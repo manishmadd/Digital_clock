a7e39eff6881c9db4f03936a92ea6ef1ceb5a905
manishmadd <manishmaddeshiya9454@gmail.com>
2022-07-09 20:17:35 +0530

    added a welcome screeen

--- digitalclock.py
+++ digitalclock.py
@@ -4,6 +4,6 @@ from tkinter import Tk, Label
 window =Tk()
 window.title("Digital Clock")
 window.configure(bg="steelblue")
-label=Label(window,  font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")
+label=Label(window, text="welcome" ,font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 window.mainloop()
