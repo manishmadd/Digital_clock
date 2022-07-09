c451be378ef93d0ea0bc7fe7e61ad76df17f9d52
manishmadd <manishmaddeshiya9454@gmail.com>
2022-07-09 20:16:16 +0530

    added a welcom msg

--- digitalclock.py
+++ digitalclock.py
@@ -1,18 +1,9 @@
-from cProfile import label
-from struct import pack
 from tkinter import Tk, Label
+
 
 window =Tk()
 window.title("Digital Clock")
-window.geometry("600×300")
 window.configure(bg="steelblue")
-label=Label(window, text="welcome", font=("Arial Black", 78, "bold"), bg="stellblue", fg="whiteblue")
+label=Label(window,  font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")
 label.pack(pady=100)
 window.mainloop()
-
-
-
-
-
-
-
